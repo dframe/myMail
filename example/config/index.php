52eaeeea477837e08befceb432c38faaa580d19d
Slawek Kaleta <s.kaleta@citycore.pro>
2016-03-29 22:02:28 +0200

    poprawa exampla

---
+++ example/config/index.php
@@ -0,0 +1,11 @@
+<?php
+use \Dframe\MyMail;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$config = require_once 'config/config.php'; 
+
+$myMail = new \MyMail($config); // Załadowanie Configu
+$addAddress[] = array('mail' => 'adres@email', 'name' => 'titleFrom'); // Adresy na jakie ma wysłać
+$mail->send($addAddress, 'Test Mail', $body);
+
+?>
