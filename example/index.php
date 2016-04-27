88b47d079fb71336af7a6886250d0fa3417de4ab
Slawek Kaleta <s.kaleta@citycore.pro>
2016-04-27 14:10:22 +0200

    Poprawa Configu

--- example/index.php
+++ example/index.php
@@ -4,7 +4,7 @@ use \Dframe\MyMail;
 require_once __DIR__ . '/../vendor/autoload.php';
 $config = require_once 'config/config.php'; 
 
-$myMail = new \MyMail($config); // Załadowanie Configu
+$myMail = new \MyMail(); // Załadowanie Configu
 $addAddress[] = array('mail' => 'adres@email', 'name' => 'titleFrom'); // Adresy na jakie ma wysłać
 $mail->send($addAddress, 'Test Mail', $body);
 
