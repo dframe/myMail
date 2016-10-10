28286adff2d5b641956b5e836bfc986a65e8a3f4
Dusta <dusta@users.noreply.github.com>
2016-10-10 15:37:30 +0200

    Remove isSMTP();

--- src/myMail.php
+++ src/myMail.php
@@ -9,64 +9,63 @@ use \Dframe\Config;
 */
 class myMail
 {
-	
-	public $addAttachment = array();
-	//protected $config;
+    
+    public $addAttachment = array();
+    //protected $config;
 
-	public function __construct($config = null){
-		$this->config = $config;
-		
-		if(is_null($this->config))
-			$this->config = Config::load('myMail')->get();
+    public function __construct($config = null){
+        $this->config = $config;
+        
+        if(is_null($this->config))
+            $this->config = Config::load('myMail')->get();
 
         $this->mailObject = new \PHPMailer;
         $this->mailObject->CharSet = 'UTF-8';
-        $this->mailObject->isSMTP();      
-		$this->mailObject->Host = implode(';', $this->config['Hosts']);
-		$this->mailObject->SMTPAuth = $this->config['SMTPAuth'];
-		$this->mailObject->Username = $this->config['Username'];
-		$this->mailObject->Password = $this->config['Password'];
-		$this->mailObject->SMTPSecure = $this->config['SMTPSecure'];
-		$this->mailObject->Port = $this->config['Port'];
-		return $this->mailObject;
+        $this->mailObject->Host = implode(';', $this->config['Hosts']);
+        $this->mailObject->SMTPAuth = $this->config['SMTPAuth'];
+        $this->mailObject->Username = $this->config['Username'];
+        $this->mailObject->Password = $this->config['Password'];
+        $this->mailObject->SMTPSecure = $this->config['SMTPSecure'];
+        $this->mailObject->Port = $this->config['Port'];
+        return $this->mailObject;
 
-	}
+    }
 
-	public function addAttachment($addAttachment){
-		$this->addAttachment[] = $addAttachment;
-		return $this;
-	}
+    public function addAttachment($addAttachment){
+        $this->addAttachment[] = $addAttachment;
+        return $this;
+    }
 
-	public function send($Recipient = array(), $Subject, $Body, $Sender = ''){
+    public function send($Recipient = array(), $Subject, $Body, $Sender = ''){
 
-		$this->mailObject->ClearAddresses();  // each AddAddress add to list
-		$this->mailObject->ClearCCs();
-		$this->mailObject->ClearBCCs();
+        $this->mailObject->ClearAddresses();  // each AddAddress add to list
+        $this->mailObject->ClearCCs();
+        $this->mailObject->ClearBCCs();
 
 
-		if($Sender != '')
-			$this->mailObject->setFrom($Sender['address'], $Sender['name']);
-		else
-			$this->mailObject->setFrom($this->config['senderMail'], $this->config['senderName']);
-		
+        if($Sender != '')
+            $this->mailObject->setFrom($Sender['address'], $Sender['name']);
+        else
+            $this->mailObject->setFrom($this->config['senderMail'], $this->config['senderName']);
+        
         
         $this->mailObject->Subject = $Subject;
         $this->mailObject->addAddress($Recipient['mail'], $Recipient['name']);     // Add a recipient
 
         if(!empty($this->addAttachment))
-        	foreach ($addAttachment as $key => $Attachment) {
-        		 $this->mailObject->addAttachment = $Attachment;
-        	}
+            foreach ($addAttachment as $key => $Attachment) {
+                 $this->mailObject->addAttachment = $Attachment;
+            }
         $this->mailObject->msgHTML($Body);
 
-    	if (!$this->mailObject->send()) {
-		    throw new \Exception("Mailer Error: " . $this->mailObject->ErrorInfo);
-		    return false;
-		}
+        if (!$this->mailObject->send()) {
+            throw new \Exception("Mailer Error: " . $this->mailObject->ErrorInfo);
+            return false;
+        }
 
-		$this->addAttachment = null;
-		return true;
-	}
+        $this->addAttachment = null;
+        return true;
+    }
 
 
 }
