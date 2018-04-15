17165b54b5576538329017a57d9c5515202c3af6
Dusta <dusta@users.noreply.github.com>
2018-04-15 20:26:15 +0200

    Update myMail.php

--- examples/example1/config/myMail.php
+++ examples/example1/config/myMail.php
@@ -1,18 +1,18 @@
 <?php
 
 return array(
-    'Hosts' => array('example@mail'), // Specify main and backup SMTP servers
+    'Hosts' => array('example@mail'),      // Specify main and backup SMTP servers
     'SMTPAuth' => true,                    // Enable SMTP authentication
-    'Username' => 'Username@mail',    // SMTP username
-    'Password' => '',                // SMTP password
-    'SMTPSecure' => 'tls',                // Enable TLS encryption, `ssl` also accepted
-    'Port' => 587,                        // Port
+    'Username' => 'Username@mail',         // SMTP username
+    'Password' => '',                      // SMTP password
+    'SMTPSecure' => 'tls',                 // Enable TLS encryption, `ssl` also accepted
+    'Port' => 587,                         // Port
 
-    'setMailTemplateDir' => './View/templates/mail',
+    'setMailTemplateDir' => APP_DIR.'View/templates/mail',
     'smartyHtmlExtension' => '.html.php',              // Default '.html.php'
-    'smartyTxtExtension' => '.txt.php',    //Default '.txt.php'
+    'smartyTxtExtension' => '.txt.php',                // Default '.txt.php'
     'fileExtension' => '.html.php',
 
-    'senderName' => PROJECT_NAME, //Name of default sender
-    'senderMail' => 'senderMail@mail' //Default sender's address
+    'senderName' => PROJECT_NAME,      // Name of default sender
+    'senderMail' => 'senderMail@mail'  // Default sender's address
 );
