dc9054b5401dcc3005bf258036c4af4af4e14317
Dusta <dusta@users.noreply.github.com>
2018-04-15 20:50:40 +0200

    Update SetupTest.php

--- tests/SetupTest.php
+++ tests/SetupTest.php
@@ -7,7 +7,7 @@ if (!class_exists('\PHPUnit\Framework\TestCase') AND class_exists('\PHPUnit_Fram
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
 
-class SetupTest extends PHPUnit_Framework_TestCase {
+class SetupTest extends \PHPUnit\Framework\TestCase {
 
 
 	public function setUp(){
