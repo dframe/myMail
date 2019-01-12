33b6de80f13484b23d87956bea33e97663e1a270
Sławomir Kaleta <dusta@users.noreply.github.com>
2019-01-12 09:29:50 +0100

    Create myMail.php

---
+++ examples/example2/app/Config/myMail.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    /**
+     * Specify main and backup SMTP servers
+     */
+    'hosts' => ['primaryHostName.tld', 'backupHostName.tld'],
+
+    /**
+     * Enable SMTP authentication
+     */
+    'smtpAuth' => true,
+
+    /**
+     * SMTP username
+     */
+    'username' => 'Username@mail',
+
+    /**
+     * SMTP password
+     */
+    'password' => '',
+
+    /**
+     * Enable TLS encryption, `ssl` also accepted
+     */
+    'smtpSecure' => 'tls',
+
+    /**
+     * Port
+     */
+    'port' => 587,
+
+    /**
+     * Name of default sender
+     */
+    'senderName' => PROJECT_NAME,
+
+    /**
+     * Default sender's address
+     */
+    'senderEmail' => 'senderMail@mail'
+];
